382584dc7ff9cc68aad8736520e11347f97ddfb4
ldonle <99715408+ldonle@users.noreply.github.com>
2024-10-04 16:28:44 -0500

    Adding Virchow from slideflow as it is CC 4.0

--- slideflow_noncommercial/extractors/__init__.py
+++ slideflow_noncommercial/extractors/__init__.py
@@ -63,3 +63,8 @@ def gigapath(**kwargs):
 def virchow2(weights, **kwargs):
     from .virchow2 import Virchow2Features
     return Virchow2Features(weights, **kwargs)
+
+@register_torch
+def virchow(weights, **kwargs):
+    from .virchow import VirchowFeatures
+    return VirchowFeatures(weights, **kwargs)
